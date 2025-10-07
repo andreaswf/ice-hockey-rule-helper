25c8e419a750ccb9d1cee7f27e6dad1eb27e595d
andreaswf <awacherfuglsang@gmail.com>
2025-10-07 18:07:51 +0200

    tests for for parsing.py

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -3,19 +3,22 @@ from langchain.schema import Document
 
 
 
-
-def test_normalize_ocr_fixes_common_splits():
+# test common splits
+def test_normalize_ocr():
     s = "PENAL TY and AL TERNATE"
     out = normalize_ocr(s)
     assert "PENALTY" in out
     assert "ALTERNATE" in out
     
-    
+
+# test correct merging of pages (Documents)
 def test_merge_docs():
-    d1 = Document(page_content="Hello", metadata={'source': 'd1_source'})
+    d1 = Document(page_content="Hello ", metadata={'source': 'd1_source'})
     d2 = Document(page_content="World", metadata={'source': 'd2_source'})
     
     merged = merge_docs([d1, d2], source="merged_source")
     
-    assert merged.page_content == "HelloWorld"
-    assert merged.metadata == "merged_source"
+    assert merged[0].page_content == "Hello World"
+    assert merged[0].metadata['source'] == "merged_source"
+    
+    
